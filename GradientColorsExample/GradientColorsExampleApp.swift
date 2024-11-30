43b8b7b40833e94b354ed0fdc19524250a997b9d
Russell Gordon <rgordon@lcs.on.ca>
2024-11-30 09:43:51 -0500

    Initial Commit

---
+++ GradientColorsExample/GradientColorsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientColorsExampleApp.swift
+//  GradientColorsExample
+//
+//  Created by Russell Gordon on 2024-11-30.
+//
+
+import SwiftUI
+
+@main
+struct GradientColorsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
